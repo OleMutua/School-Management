5f64d0a132b1b23f3c8b6bf03be050ccc3eeb54e
Victor Mwenda <vmwenda.vm@gmail.com>
2017-05-08 16:03:31 +0300

    Defined separate source sets for the different outputs of the application

---
+++ app/src/lecturer/java/com/ancent/schmgmt/lecturer/LecturerMainActivity.java
@@ -0,0 +1,28 @@
+package com.ancent.schmgmt.lecturer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ancent.schmgmt.R;
+
+/**
+ * Project - school-management
+ * Package - com.ancent.schmgmt
+ * <p>
+ * Victor Mwenda
+ * +254(0)718034449
+ * vmwenda.vm@gmail.com
+ * <p>
+ * Android App Development Laptop
+ * Created by victor on 5/8/2017 at 3:49 PM.
+ */
+
+public class LecturerMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
